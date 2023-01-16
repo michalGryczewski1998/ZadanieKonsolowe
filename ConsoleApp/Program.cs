d3edf4db0efe6d8227bcaf02c8fbb955cbbaa4c3
Michał Gryczewski <michal12083@outlook.com>
2023-01-16 16:25:18 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿class ConsoleApp
+{
+    static void Main(string[] args)
+    {
+        PobracDane();
+    }
+
+    private static void PobracDane()
+    {
+        Console.WriteLine("Podaj liczbę (10-100)");
+        _ = int.TryParse(Console.ReadLine(), out int liczba);
+
+        if (liczba >= 10 && liczba <= 100)
+        {
+            Obliczenia(liczba);
+        }
+        else
+        {
+            Console.WriteLine("Podaj liczbę z przedziału 10-100");
+            PobracDane();
+        }
+    }
+
+    private static void Obliczenia(int liczba)
+    {
+        int wynik = 0;
+        List<int> list = new();
+
+        for (int i = 0; i < liczba; i++)
+        {
+            if (i % 2 != 0)
+            {
+                wynik += i;
+                list.Add(i);
+            }
+        }
+        Console.WriteLine("Wynik: " + wynik);
+
+        ZapisDoPliku(list);
+    }
+
+    private static void ZapisDoPliku(List<int> listaDanych)
+    {
+        try
+        {
+            TextWriter writer = new StreamWriter("log.txt");
+
+            foreach (var st in listaDanych)
+            {
+                writer.WriteLine(st);
+            }
+
+            writer.Close();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Błąd podczas zapisu do pliku.");
+        }
+    }
+}
